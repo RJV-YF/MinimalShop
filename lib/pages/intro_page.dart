3e7d2a232ff09ed79fb0cd31deb6057fa6de366c
RJV-YF <freeworld.creator01@gmail.com>
2024-08-19 12:25:12 +0530

    Created the pages directory and themes directory configured the theme with the material app created an intro page as an entry point to the app

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: const Column(
+        children: [
+          // logo
+
+          // title
+
+          // subtitle
+
+          // button
+        ],
+      ),
+    );
+  }
+}
